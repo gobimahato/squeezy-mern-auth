c496084ee155f3de398aeddf560a5c33ab5b89dd
Gobi Mahato <gobimahato@gmail.com>
2025-04-08 18:59:23 +0530

    feat: initial backend setup for authentication API

---
+++ backend/src/index.ts
@@ -0,0 +1,29 @@
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import cors from "cors";
+import express, { type Request, type Response } from "express";
+import { config } from "./config/app.config";
+
+const app = express();
+// const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: config.APP_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Hello Express.js",
+  });
+});
+
+app.listen(config.PORT, async () => {
+  console.log(`Server litensing on port ${config.PORT} in ${config.NODE_ENV}`);
+});
